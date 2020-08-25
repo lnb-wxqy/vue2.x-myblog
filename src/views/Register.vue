3c84f5a84080f6ce0de5ebc9f1e2dcc58f19b53b
lnb-wxqy <905119087@qq.com>
2020-08-25 08:44:35 +0800

    add telephone validate

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,54 +1,55 @@
 <template>
   <div class="register">
     <b-row class="mt-5">
-      <b-col md="8"
-             offset-md="2"
-             lg="6"
-             offset-lg="3">
+      <b-col md="8" offset-md="2" lg="6" offset-lg="3">
         <b-card title="注册">
-
           <b-form>
             <!-- 姓名 -->
             <b-form-group label="姓名">
-              <b-form-input v-model="user.name"
-                            type="text"
-                            placeholder="请输入用户名">
+              <b-form-input
+                v-model="user.name"
+                type="text"
+                placeholder="请输入用户名"
+              >
               </b-form-input>
             </b-form-group>
 
             <!-- //手机号 -->
             <b-form-group label="手机号">
-              <b-form-input v-model="user.telephone"
-                            type="text"
-                            placeholder="请输入手机号"
-                            :state="validateState('telephone')">
+              <b-form-input
+                v-model="user.telephone"
+                type="text"
+                placeholder="请输入11位手机号"
+                :state="validateState('telephone')"
+              >
               </b-form-input>
               <b-form-invalid-feedback :state="validateState('telephone')">
-                手机号不符合要求
+                手机号必须为11位
               </b-form-invalid-feedback>
             </b-form-group>
 
             <!-- 密码 -->
             <b-form-group label="密码">
-              <b-form-input v-model="user.password"
-                            type="password"
-                            placeholder="输入密码）"
-                            :state="validateState('password')"></b-form-input>
+              <b-form-input
+                v-model="user.password"
+                type="password"
+                placeholder="请输入密码(不能少于6位)"
+                :state="validateState('password')"
+              ></b-form-input>
               <b-form-invalid-feedback :state="validateState('password')">
                 密码必须大于等于 6 位
               </b-form-invalid-feedback>
             </b-form-group>
 
             <b-form-group>
-              <b-button @click="register"
-                        variant="outline-primary"
-                        block>注册</b-button>
+              <b-button @click="register" variant="outline-primary" block
+                >注册</b-button
+              >
             </b-form-group>
           </b-form>
         </b-card>
       </b-col>
     </b-row>
-
   </div>
 </template>
 
@@ -57,27 +58,32 @@
 // import { BIconArrowDown } from 'bootstrap-vue'
 
 export default {
-    components: {
+  components: {
     // BIcon,
     // BIconArrowUp,
     // BIconArrowDown
-    },
-    data () {
-        return {
-            user: {
-                name: '',
-                telephone: '',
-                password: ''
-            }
-        }
-    },
-    methods: {
-        validateState (telephone) {
-            // TODO
-        }
+  },
+  data() {
+    return {
+      user: {
+        name: '',
+        telephone: '',
+        password: ''
+      },
+      showTelephoneValidater: false
     }
+  },
+  methods: {
+    register() {
+      if (this.user.telephone.length != 11) {
+        this.showTelephoneValidater = true
+      }
+      console.log(this.user.telephone)
+      console.log(this.showTelephoneValidater)
+    },
+    validateState(telephone) {}
+  }
 }
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
--- src/views/layout/Navbar.vue
+++ src/views/layout/Navbar.vue
@@ -1,9 +1,8 @@
 <template>
   <div>
-    <b-navbar toggleable="lg"
-              type="dark"
-              variant="info">
-      <b-navbar-brand>五行缺雨 <b-icon icon="alarm-fill"></b-icon>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand
+        >五行缺雨 <b-icon icon="alarm-fill"></b-icon>
         <!-- <b-row class="text-md-center">
           <b-col md="6"
                  class="mb-3">
@@ -14,13 +13,18 @@
         </b-row> -->
       </b-navbar-brand>
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-      <b-collapse id="nav-collapse"
-                  is-nav>
+      <b-collapse id="nav-collapse" is-nav>
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
           <b-navbar-nav>
-            <b-nav-item>登录</b-nav-item>
-            <b-nav-item>注册</b-nav-item>
+            <!-- // 导航到路由名为login的页面和register的页面 -->
+            <!-- 此处name为 router 目录下 index.js中路由的名字name -->
+            <b-nav-item @click="$router.replace({ name: 'Login' })"
+              >登录</b-nav-item
+            >
+            <b-nav-item @click="$router.replace({ name: 'Register' })"
+              >注册</b-nav-item
+            >
           </b-navbar-nav>
         </b-navbar-nav>
       </b-collapse>
